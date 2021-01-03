aa8bced3db05ee6cfdcd630b047ab2cb35e763c0
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-02 22:56:33 -0500

    started with authentifications

--- app.js
+++ app.js
@@ -2,12 +2,26 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 const authRouter = require('./routers/auth.js');
 const analyticsRouter = require('./routers/analytics.js');
 const categoryRouter = require('./routers/category.js');
 const orderRouter = require('./routers/order.js');
 const positionRouter = require('./routers/position.js');
+// const keys = require('./config/keys');
 const app = express();
+
+// for production
+// mongoose
+//   .connect(keys.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log('mongo was connected'))
+//   .catch((err) => console.log('err', err));
+
+// for development
+mongoose
+  .connect('mongodb://localhost:27017/school', { useNewUrlParser: true })
+  .then(() => console.log('mongo connected'))
+  .catch((err) => console.log('bad connection'));
 
 app.use(morgan('dev'));
 app.use(cors());
--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,68 @@
-module.exports.login = function (req, res) {
-  const login = req.body.login;
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+const keys = require('../config/keys');
+
+module.exports.login = async function (req, res) {
+  const email = req.body.email;
   const password = req.body.password;
-  res.status(200).json({
-    login,
-    password,
-  });
+  // check user by email
+  const candidate = await User.findOne({ email: email });
+  if (candidate) {
+    // check user's password
+    const passwordResult = bcrypt.compareSync(password, candidate.password);
+    if (passwordResult) {
+      // if success - send token to user
+      const token = jwt.sign(
+        {
+          email: candidate.email,
+          userId: candidate._id,
+        },
+        keys.jwt,
+        { expiresIn: 60 * 60 }
+      );
+
+      res.status(200).json({ token: `Bearer ${token}` });
+    } else {
+      res.status(401).json({ message: 'Something was wrong. Try again.' });
+    }
+  } else {
+    res.status(404).json({ message: 'User not found' });
+  }
 };
 
-module.exports.register = function (req, res) {
-  res.status(200).json({
-    register: true,
+module.exports.register = async function (req, res) {
+  // email , password
+  // if existed through error
+  const candidate = await User.findOne({ email: req.body.email });
+  if (candidate) {
+    return res
+      .status(409)
+      .json({ message: 'This email was used.Try an another one' });
+  } else {
+    // will create secured password
+    // bcrypt
+    const salt = bcrypt.genSaltSync(10);
+    const password = req.body.password;
+    const user = new User({
+      email: req.body.email,
+      password: bcrypt.hashSync(password, salt),
+    });
+
+    // save to Dbase
+    try {
+      await user.save();
+
+      // response with status "created successfully"
+      res.status(201).json({ message: 'User was created' });
+    } catch (error) {
+      // through error in handlers
+    }
+  }
+  const user = new User({
+    email: req.body.email,
+    password: req.body.email,
   });
+
+  user.save().then(() => console.log('created user'));
 };
