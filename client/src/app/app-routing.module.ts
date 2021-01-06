d82add9944711a0f633ac4a8895160e33b6d1689
Yurii Tym <yuriy.tim@yahoo.com>
2021-01-05 23:08:38 -0500

    client side and login-page in angular

---
+++ client/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { RegisterPageComponent } from './register-page/register-page.component';
+import { SiteLayoutComponent } from './shared/layouts/site-layout/site-layout.component';
+import { AuthLayoutComponent } from './shared/layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: '', component: AuthLayoutComponent, children: [{path: '', redirectTo: '/login', pathMatch: 'full'},
+    {path: 'login', component: LoginPageComponent}, {path: 'register', component: RegisterPageComponent}]}, {
+    path: '', component: SiteLayoutComponent, children: []
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
